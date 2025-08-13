bae0f92ef443849c2e598404f27354a143450fb0
vanshika parashar <parasharvanshika2005@gmail.com>
2025-08-13 10:55:13 +0530

    first commit

---
+++ practise_problem1/p1.java
@@ -0,0 +1,12 @@
+package bridgelabz.practise_problem1;
+import java.util.*;
+
+public class p1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    
+        
+        }
+    }
+    
+
